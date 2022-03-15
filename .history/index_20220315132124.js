538044cc42086892ab929192a34a3a4ff9a37ac0
Mario Capitbrok <mario@capitbrok.com>
2022-03-15 13:22:20 -0600

    3.4 Phonebook backend step4

---
+++ .history/index_20220315132124.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const app = express()
+
+let entries = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+]
+
+const date = new Date()
+
+app.get('/info', (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${entries.length} people<p>` + `<p>${date}</p>`
+  )
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(entries)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const entry = entries.find((e) => e.id === id)
+
+  if (entry) {
+    response.json(entry)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  entries = entries.filter((e) => e.id !== id)
+
+  response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
