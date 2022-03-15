5f0c5e3b1da96dd16eeacc1ae4707958e1b87777
Mario Capitbrok <mario@capitbrok.com>
2022-03-15 10:40:30 -0600

    3.2 Phonebook backend step2

---
+++ .history/index_20220315103831.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+
+let entries = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+]
+
+const date = new Date()
+
+app.get('/info', (req, res) => {
+  res.send(
+    `<p>Phonebook has info for ${entries.length} people<p>` + `<p>${Date()}</p>`
+  )
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(entries)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
